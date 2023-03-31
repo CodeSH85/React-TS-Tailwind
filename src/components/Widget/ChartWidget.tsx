ed53f710caf8a7994e7701fa3aa6b34637667b64
CodeSH85 <codesh85@gmail.com>
2023-03-31 23:57:05 +0800

    add react-chartjs-2

--- src/components/Widget/ChartWidget.tsx
+++ src/components/Widget/ChartWidget.tsx
@@ -1,47 +1,73 @@
 import { useRef, useEffect } from "react";
-import { Chart } from "chart.js/auto";
 import { Chart_Props } from "./type";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  indexAxis: 'y' as const,
+  elements: {
+    bar: {
+      borderWidth: 2,
+    },
+  },
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'right' as const,
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Horizontal Bar Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: [100, 300, 420, 120],
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: [200, 50, 200, 90],
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
 
 const ChartWidget = (props: Chart_Props) => {
-
-  const chartRef = useRef<HTMLCanvasElement | null>(null);
-  const myChartRef = chartRef.current?.getContext('2d');
-  
-  useEffect(() => {
-    if (myChartRef) {
-      const config = {
-        type: 'bar',
-        data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true
-            }
-          }
-        }
-      };
-      new Chart(myChartRef, config);
-    }
-  }, []);
-
 
   return(
     <div className="container">
       <div className="">
-        Data Title
-      </div>
-        <canvas ref={chartRef}></canvas>
-      <div className="w-64 h-64">
+        <Bar options={options} data={data} />
       </div>
     </div>
   )
-}
+};
 
 export default ChartWidget;
