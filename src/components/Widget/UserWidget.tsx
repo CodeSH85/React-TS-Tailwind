db220219a98a7f1eb43c6fcac9091454faee8518
CodeSH85 <codesh85@gmail.com>
2023-04-27 23:38:11 +0800

    add i18n

--- src/components/Widget/UserWidget.tsx
+++ src/components/Widget/UserWidget.tsx
@@ -1,8 +1,35 @@
 import React, { useState } from "react";
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import { Buttons } from "../UI/index";
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+    resources: {
+      en: {
+        translation: {
+          "Welcome": "歡迎"
+        }
+      }
+    },
+    lng: "en", // if you're using a language detector, do not define the lng option
+    fallbackLng: "en",
+
+    interpolation: {
+      escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    }
+  });
+
+
 
 const Widget = () => {
 
   const user:string = 'Saul';
+  const { t } = useTranslation();
 
   return (
     <div className="flex items-center">
@@ -14,9 +41,11 @@ const Widget = () => {
       </div>
       <div className="ml-3">
         <div className="text-lg">
-          Welcome<span>{user}</span>
+          {t('Welcome')}<span>{user}</span>
         </div>
         <div className="text-sm">Developer</div>
+        <Buttons type={'primary'}>Test</Buttons>
+        <Buttons type={'danger'}>Test</Buttons>
       </div>
     </div>
   )  
