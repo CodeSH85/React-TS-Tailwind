b51b6a03a8942aefbff6c879104f029b6538bcfd
CodeSH85 <codesh85@gmail.com>
2024-06-29 16:34:50 +0800

    Chore: Add classnames lib

--- src/components/UI/Tabs/Tabs.tsx
+++ src/components/UI/Tabs/Tabs.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import classnames  from "classnames";
 
-type TabItem = {
+export type TabItem = {
   title: string;
   key: string | number;
 };
 
-type TabsProps = {
+export type TabsProps = {
   items: TabItem[];
   children?: React.ReactNode;
   closable?: boolean;
@@ -14,53 +15,49 @@ type TabsProps = {
 };
 
 const Tabs = (props: TabsProps) => {
-  const {items, closable, children, onCloseTab} = props;
+  const { items, closable, children, onCloseTab } = props;
   const [tabs, setTabs] = useState<TabItem[]>(items);
   const [curTab, setCurTab] = useState<TabItem>();
+
   function onClick(tabItem: TabItem) {
     setCurTab(tabItem);
   }
-  function handleCloseTab(e: React.MouseEvent<HTMLElement>, key: TabItem['key']) {
+  function handleCloseTab(
+    e: React.MouseEvent<HTMLElement>,
+    key: TabItem["key"]
+  ) {
     if (onCloseTab) onCloseTab(e, key);
-    const newTabs = tabs.filter(tab => tab.key !== key);
+    const newTabs = tabs.filter((tab) => tab.key !== key);
     setTabs(newTabs);
+  }
+  function isActive(key: TabItem["key"]) {
+    return curTab?.key === key;
   }
   const tabItem = (item: TabItem) => {
     return (
       <div
         key={item.key}
         onClick={() => onClick(item)}
-        className={[
-          curTab?.key === item.key && "--active",
-          'bg-slate-300'
-        ].join(' ')}
+        className={classnames([
+          isActive(item.key) ? "bg-white" : "bg-slate-100",
+          "px-2 py-1 rounded cursor-pointer",
+        ])}
       >
-        { 
-          children ??
+        {children ?? (
           <>
             <span>{item.title}</span>
-            {
-              closable &&
-              <button
-                onClick={e => handleCloseTab(e, item.key)}
-              >
-              X
-              </button>
-            }
+            {closable && (
+              <button onClick={(e) => handleCloseTab(e, item.key)}>X</button>
+            )}
           </>
-        }
+        )}
       </div>
-    )
-  }
+    );
+  };
 
   return (
-    <nav className="flex">
-      {tabs.length &&
-        tabs.map((item) => {
-          return (
-            tabItem(item)
-          );
-        })}
+    <nav className="flex w-full bg-slate-200">
+      {tabs.length && tabs.map((item) => tabItem(item))}
     </nav>
   );
 };
